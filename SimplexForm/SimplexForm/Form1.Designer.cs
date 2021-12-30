95e5007ef61a34dfcd72b76c357c535a03d18809
Büşra Akay <61781887+busraakay@users.noreply.github.com>
2021-12-30 15:33:14 +0300

    add files

---
+++ SimplexForm/SimplexForm/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace SimplexForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.degisken_txt = new System.Windows.Forms.TextBox();
+            this.kisit_txt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Değişken Sayısı Giriniz:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kısıt Sayısı Giriniz:";
+            // 
+            // degisken_txt
+            // 
+            this.degisken_txt.Location = new System.Drawing.Point(315, 41);
+            this.degisken_txt.Name = "degisken_txt";
+            this.degisken_txt.Size = new System.Drawing.Size(100, 20);
+            this.degisken_txt.TabIndex = 2;
+            // 
+            // kisit_txt
+            // 
+            this.kisit_txt.Location = new System.Drawing.Point(315, 74);
+            this.kisit_txt.Name = "kisit_txt";
+            this.kisit_txt.Size = new System.Drawing.Size(100, 20);
+            this.kisit_txt.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Onayla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox1.Location = new System.Drawing.Point(45, 164);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(370, 88);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Zmax Fonksiyonu";
+            this.groupBox1.Visible = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox2.Location = new System.Drawing.Point(45, 285);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(370, 230);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kısıtlar";
+            this.groupBox2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(315, 527);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Hesapla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.Info;
+            this.groupBox3.Location = new System.Drawing.Point(45, 575);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(370, 132);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sonuç";
+            this.groupBox3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 733);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.kisit_txt);
+            this.Controls.Add(this.degisken_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox degisken_txt;
+        private System.Windows.Forms.TextBox kisit_txt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ SimplexForm/SimplexForm/Form1.cs
@@ -0,0 +1,503 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimplexForm
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Kisit
+        {
+            public List<double> kisit = new List<double>();
+            public double kisitDegeri;
+            public double amacKatsayisi = 0;
+
+        } 
+
+        public class Simplex
+        {
+            public static int kisitSayisi;
+            public static int degiskenSayisi;
+
+            public List<double> cj = new List<double>();
+            public List<double> zj = new List<double>();
+            public double zjDegeri = 0;
+            public List<double> optimumKontrolu = new List<double>();
+            public int tabloSayisi = 0;
+
+            public bool optimumMu;
+
+            public int anahtarSutunIndex = 0;
+            public int anahtarSatirIndex = 0;
+            public double anahtarSayi;
+            public List<double> anahtarSatir = new List<double>();
+
+            public Kisit[] kisitlar = new Kisit[kisitSayisi];
+
+            public Simplex(double[] _cj)
+            {
+                for (int i = 0; i < kisitSayisi; i++)
+                {
+                    kisitlar[i] = new Kisit();
+
+                }
+                cj.AddRange(_cj);
+            }
+
+
+            public void DolguEkle()
+            {
+                for (int i = 0; i < kisitSayisi; i++)
+                {
+                    for (int j = 0; j < kisitSayisi; j++)
+                    {
+                        if (j == i)
+                        {
+                            kisitlar[i].kisit.Add(1);
+                            cj.Add(0);
+                        }
+                        else
+                        {
+                            kisitlar[i].kisit.Add(0);
+                        }
+                    }
+                }
+
+
+                for (int j = 0; j < kisitSayisi + degiskenSayisi; j++)
+                {
+                    zj.Add(0);
+                    optimumKontrolu.Add(0);
+                }
+
+            }
+
+            public void zjHesapla()
+            {
+                double toplam = 0;
+
+
+                for (int j = 0; j < kisitSayisi + degiskenSayisi; j++)
+                {
+                    for (int k = 0; k < kisitSayisi; k++)
+                    {
+                        toplam = toplam + kisitlar[k].kisit[j] * kisitlar[k].amacKatsayisi;
+                    }
+                    zj[j] = toplam;
+                    toplam = 0;
+                }
+
+                double temp = 0;
+                for (int i = 0; i < kisitSayisi; i++)
+                {
+                    temp += kisitlar[i].kisitDegeri * kisitlar[i].amacKatsayisi;
+                }
+                zjDegeri = temp;
+
+
+            }
+
+            public bool OptimumHesapla()
+            {
+                double enBuyuk = 0;
+
+
+                for (int i = 0; i < kisitSayisi + degiskenSayisi; i++)
+                {
+                    optimumKontrolu[i] = cj[i] - zj[i];
+                }
+
+
+                for (int i = 0; i < optimumKontrolu.Count; i++)
+                {
+                    if (optimumKontrolu[i] > enBuyuk)
+                    {
+                        enBuyuk = optimumKontrolu[i];
+                    }
+
+                }
+                anahtarSutunIndex = optimumKontrolu.IndexOf(enBuyuk);
+
+                for (int i = 0; i < kisitSayisi + degiskenSayisi; i++)
+                {
+                    if (optimumKontrolu[i] > 0)
+                    {
+                        optimumMu = false;
+                        break;
+                    }
+                    else
+                    {
+                        optimumMu = true;
+                    }
+
+                }
+
+
+                return optimumMu;
+
+            }
+
+            public void TabloOlustur()
+            {
+                DolguEkle();
+                zjHesapla();
+                OptimumHesapla();
+                tabloSayisi++;
+                
+
+                do
+                {
+                    tabloSayisi++;
+                    double enKucuk = kisitlar[0].kisit[anahtarSutunIndex];
+                    for (int i = 0; i < kisitSayisi; i++)
+                    {
+                        if ((kisitlar[i].kisitDegeri / kisitlar[i].kisit[anahtarSutunIndex]) < enKucuk)
+                        {
+                            enKucuk = kisitlar[i].kisitDegeri / kisitlar[i].kisit[anahtarSutunIndex];
+                            anahtarSatirIndex = i;
+                        }
+                    }
+
+                    anahtarSayi = kisitlar[anahtarSatirIndex].kisit[anahtarSutunIndex];
+
+                    List<double> temp = new List<double>();
+
+                    for (int i = 0; i < kisitSayisi + degiskenSayisi; i++)
+                    {
+                        temp.Add(kisitlar[anahtarSatirIndex].kisit[i] / anahtarSayi);
+
+                    }
+                    temp.Add(kisitlar[anahtarSatirIndex].kisitDegeri / anahtarSayi);
+
+
+                    double geciciAnahtarSayi = 0;
+
+
+
+                    for (int i = 0; i < kisitSayisi; i++)
+                    {
+                        geciciAnahtarSayi = kisitlar[i].kisit[anahtarSutunIndex];
+
+                        if (i != anahtarSatirIndex)
+                        {
+                            kisitlar[i].kisitDegeri = kisitlar[i].kisitDegeri - (temp[degiskenSayisi + kisitSayisi] * geciciAnahtarSayi);
+                        }
+                        else
+                        {
+                            kisitlar[i].kisitDegeri = kisitlar[i].kisitDegeri / anahtarSayi;
+                        }
+                        for (int j = 0; j < kisitSayisi + degiskenSayisi; j++)
+                        {
+                            if (i == anahtarSatirIndex)
+                            {
+                                kisitlar[i].kisit[j] = temp[j];
+
+                            }
+                            else
+                            {
+                                kisitlar[i].kisit[j] = kisitlar[i].kisit[j] - (temp[j] * geciciAnahtarSayi);
+                            }
+                        }
+                    }
+
+                    kisitlar[anahtarSatirIndex].amacKatsayisi = cj[anahtarSutunIndex];
+
+
+                    zjHesapla();
+                    OptimumHesapla();
+                } while (!optimumMu);
+            }
+
+
+
+        }
+
+        
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        
+        TextBox[] TextZmax;
+        TextBox[,] textKisit;
+        TextBox[] textKisitDegeri;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Label[] LabelZmax;
+
+            int kisitSayisi = int.Parse(kisit_txt.Text);
+            int degiskenSayisi = int.Parse(degisken_txt.Text);
+
+                      
+            
+            int left = 15;
+            int top = 25;
+
+
+            LabelZmax = new Label[degiskenSayisi];
+            int x = 1;
+            for (int i = 0; i < degiskenSayisi; i++)
+            {
+                LabelZmax[i] = new Label();
+                LabelZmax[i].Left = left;
+                LabelZmax[i].Top = top;
+                LabelZmax[i].Width = 50;
+                LabelZmax[i].Text = "x"+x;
+                x++;
+                groupBox1.Controls.Add(LabelZmax[i]);
+                left += 50;
+            }
+
+            top +=25;
+            left = 15;
+            TextZmax = new TextBox[degiskenSayisi];
+            
+            for (int i = 0; i < degiskenSayisi; i++)
+            {
+                TextZmax[i] = new TextBox();
+                TextZmax[i].Left = left;
+                TextZmax[i].Top = top;
+                TextZmax[i].Width = 50;
+                x++;
+                groupBox1.Controls.Add(TextZmax[i]);
+                left += 50;
+            }
+            groupBox1.Visible = true;
+
+            Label isaret = new Label();
+            Label miktar = new Label();
+
+            left = 15;
+            top = 25;
+            x = 1;
+            for (int i = 0; i < degiskenSayisi + 2; i++)
+            {
+                if(i == degiskenSayisi)
+                {
+                    isaret.Left = left;
+                    isaret.Top = top;
+                    isaret.Width = 50;
+                    isaret.Text = "İşaret";
+                    groupBox2.Controls.Add(isaret);
+                    left += 50;
+                    continue;
+                }
+                if(i == degiskenSayisi + 1)
+                {
+                    miktar.Left = left;
+                    miktar.Top = top;
+                    miktar.Width = 50;
+                    miktar.Text = "Miktar";
+                    groupBox2.Controls.Add(miktar);
+                    left += 50;
+                    continue;
+                }
+                LabelZmax[i] = new Label();
+                LabelZmax[i].Left = left;
+                LabelZmax[i].Top = top;
+                LabelZmax[i].Width = 50;
+                LabelZmax[i].Text = "x" + x;
+                x++;
+                groupBox2.Controls.Add(LabelZmax[i]);
+                left += 50;
+            }
+            
+
+            left = 15;
+            top += 25;
+            
+            TextBox textIsaret;
+            textKisit = new TextBox[kisitSayisi,degiskenSayisi];
+            textKisitDegeri = new TextBox[degiskenSayisi];
+
+            int kisitDegeriSayaci = 0;
+            for (int i = 0; i < kisitSayisi; i++)
+            {
+                for (int j = 0; j < degiskenSayisi + 2; j++)
+                {
+                    if (j == degiskenSayisi)
+                    {
+                        textIsaret = new TextBox();
+                        textIsaret.Left = left;
+                        textIsaret.Top = top;
+                        textIsaret.Width = 50;
+                        textIsaret.Text = "<=";
+                        groupBox2.Controls.Add(textIsaret);
+                        left += 50;
+                        continue;
+
+                    }
+                    if (j == degiskenSayisi + 1)
+                    {
+                        textKisitDegeri[kisitDegeriSayaci] = new TextBox();
+                        textKisitDegeri[kisitDegeriSayaci].Left = left;
+                        textKisitDegeri[kisitDegeriSayaci].Top = top;
+                        textKisitDegeri[kisitDegeriSayaci].Width = 50;
+                        groupBox2.Controls.Add(textKisitDegeri[kisitDegeriSayaci]);
+                        kisitDegeriSayaci++;
+                        left += 50;
+                        continue;
+
+                    }
+                    textKisit[i, j] = new TextBox();
+                    textKisit[i, j].Left = left;
+                    textKisit[i, j].Top = top;
+                    textKisit[i, j].Width = 50;
+                    groupBox2.Controls.Add(textKisit[i, j]);
+
+                    left += 50;
+                }
+                top += 25;
+                left = 15;
+            }
+            groupBox2.Visible = true;
+            button2.Visible = true;
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Simplex.kisitSayisi = int.Parse(kisit_txt.Text);
+            Simplex.degiskenSayisi = int.Parse(degisken_txt.Text);
+            double[] cj = new double[Simplex.degiskenSayisi];
+
+
+            for (int i = 0; i < Simplex.degiskenSayisi; i++)
+            {
+                cj[i] = double.Parse(TextZmax[i].Text);
+            }
+
+            Simplex s = new Simplex(cj);
+            for (int i = 0; i < Simplex.kisitSayisi; i++)
+            {
+                
+                for (int j = 0; j < Simplex.degiskenSayisi; j++)
+                {                   
+                    s.kisitlar[i].kisit.Add(double.Parse(textKisit[i, j].Text));
+                    
+                    s.kisitlar[i].kisitDegeri = double.Parse(textKisitDegeri[i].Text);
+
+                }
+            }
+
+            s.TabloOlustur();
+
+            int left = 15;
+            int top = 25;
+            int x = 1;
+            int degiskenSayisi = Simplex.degiskenSayisi;
+            Label isaret = new Label();
+            Label miktar = new Label();
+            Label[] LabelZmax = new Label[degiskenSayisi];
+            for (int i = 0; i < degiskenSayisi + 2; i++)
+            {
+                if (i == degiskenSayisi)
+                {
+                    isaret.Left = left;
+                    isaret.Top = top;
+                    isaret.Width = 50;
+                    isaret.Text = "Zmax";
+                    isaret.BackColor = Color.AntiqueWhite;
+                    groupBox3.Controls.Add(isaret);
+                    left += 50;
+                    continue;
+                }
+                if (i == degiskenSayisi + 1)
+                {
+                    miktar.Left = left;
+                    miktar.Top = top;
+                    miktar.Width = 70;
+                    miktar.Text = "Tablo Sayısı";
+                    miktar.BackColor = Color.AntiqueWhite;
+                    groupBox3.Controls.Add(miktar);
+                    left += 50;
+                    continue;
+                }
+                LabelZmax[i] = new Label();
+                LabelZmax[i].Left = left;
+                LabelZmax[i].Top = top;
+                LabelZmax[i].Width = 50;
+                LabelZmax[i].Text = "x" + x;
+                LabelZmax[i].BackColor = Color.AntiqueWhite;
+                x++;
+                groupBox3.Controls.Add(LabelZmax[i]);
+                left += 50;
+            }
+            top += 25;
+            left = 15;
+            Label Zmax = new Label();
+            Label Tablo = new Label();
+            Label[] LbllZmax = new Label[degiskenSayisi];
+            for (int i = 0; i < degiskenSayisi + 2; i++)
+            {
+                if (i == degiskenSayisi)
+                {
+                    Zmax.Left = left;
+                    Zmax.Top = top;
+                    Zmax.Width = 50;
+                    Zmax.Text = s.zjDegeri.ToString();
+                    //Zmax.Text = s.kisitlar[1].kisitDegeri.ToString();
+                    groupBox3.Controls.Add(Zmax);
+                    left += 50;
+                    continue;
+                }
+                if (i == degiskenSayisi + 1)
+                {
+                    Tablo.Left = left;
+                    Tablo.Top = top;
+                    Tablo.Width = 100;
+                    Tablo.Text = s.tabloSayisi.ToString();
+                    groupBox3.Controls.Add(Tablo);
+                    left += 50;
+                    continue;
+                }
+                LbllZmax[i] = new Label();
+                LbllZmax[i].Left = left;
+                LbllZmax[i].Top = top;
+                LbllZmax[i].Width = 50;
+                LbllZmax[i].Text = s.kisitlar[i].kisitDegeri.ToString();
+                x++;
+                groupBox3.Controls.Add(LbllZmax[i]);
+                left += 50;
+            }
+
+            groupBox3.Visible = true;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
